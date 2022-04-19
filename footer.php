52aa3438ce8100ba730a0f0da652c982167a73d2
leoxk93 <leo_xk93@hotmail.com>
2022-04-19 20:57:07 -0300

    cambios

---
+++ footer.php
@@ -0,0 +1,5 @@
+</footer>
+    <div class="whatsapp pt-3 pb-4 px-3">
+        <a href="https://api.whatsapp.com/send?phone=541162423341">
+        <i class="fa-brands fa-whatsapp"></i></a>
+    </div>
--- index.php
+++ index.php
@@ -37,9 +37,5 @@ $pg = "inicio";
             <div class="col-3"><a href="mailto:leo_xk93@hotmail.com.ar>">leo_xk93@hotmail.com.ar</a></div>
            
         </div>
-    </footer>
-    <div class="whatsapp pt-3 pb-4 px-3">
-        <a href="https://api.whatsapp.com/send?phone=541162423341">
-        <i class="fa-brands fa-whatsapp"></i></a>
-    </div>
+        <?php include_once "footer.php"; ?>
 </body>
